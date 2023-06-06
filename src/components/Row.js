a02273fd876cb171b5590d7cdfaca89ae32ba246
정지훈 <jungjihoons@naver.com>
2023-06-07 03:48:33 +0900

    #9 - 영화 자세히 보기 클릭 시 모달 생성
    영화 이미지 클릭 시 
    backdrop_path,
    title,
    overview,
    name,
    release_date,
    first_air_date,
    vote_average,
    setModalOpen
    의 내용이 담길 수 있도록 구현

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,9 +1,12 @@
 import axios from '../api/axios';
 import React, { useCallback, useEffect, useState } from 'react'
 import "./Row.css";
+import MovieModal from './MovieModal';
 
 const Row = ({ title, id, fetchUrl }) => {
   const [movies, setMovies] = useState([]);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [movieSelected, setMovieSelection] = useState({});
 
   const fetchMovieData = useCallback( async () => {
     const response = await axios.get(fetchUrl);
@@ -14,6 +17,11 @@ const Row = ({ title, id, fetchUrl }) => {
   useEffect(() => {
     fetchMovieData();
   }, [fetchMovieData])
+
+  const handelClick = (movie) => {
+    setModalOpen(true);
+    setMovieSelection(movie);
+  }
   
   return (
     <div>
@@ -34,6 +42,7 @@ const Row = ({ title, id, fetchUrl }) => {
               className='row__poster'
               src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
               alt={movie.name}
+              onClick={() => handelClick(movie)}
             />
           ))}
         </div>
@@ -46,6 +55,12 @@ const Row = ({ title, id, fetchUrl }) => {
           </span>
         </div>
       </div>
+      {modalOpen &&
+        <MovieModal
+          {...movieSelected}
+          setModalOpen={setModalOpen}
+        />
+      }
     </div>
   )
 }
