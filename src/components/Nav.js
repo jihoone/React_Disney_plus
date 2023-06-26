82e848f851af505a115eaac6ac1336504ce32597
정지훈 <jungjihoons@naver.com>
2023-06-26 12:28:32 +0900

    #17 - Firebase를 이용한 구글 로그인 로그아웃 구현
    Firebase를 이용해서 로그인, 로그아웃 환경 구성
    로그인 시 User Img값 가져와서 이미지가 보이고 로그아웃 시 로그인 페이지로 이동하게 구현
    
    https://firebase.google.com/docs/auth/web/google-signin?hl=ko#handle_the_sign-in_flow_with_the_firebase_sdk

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,12 +1,30 @@
 import React, { useEffect, useState } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom';
 import styled from 'styled-components'
+import { getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithPopup, signOut } from 'firebase/auth'
 
 const Nav = () => {
   const [show, setShow] = useState(false);
   const { pathname } = useLocation();
   const [searchValue, setSearchValue] = useState("");
   const navigate = useNavigate();
+  const auth = getAuth();
+  const provider = new GoogleAuthProvider();
+  const [userData, setUserData] = useState({});
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      console.log('user: ' + user)
+      if(user){
+        if(pathname === "/") {
+          navigate("/main")
+        }
+      }
+      else {
+        navigate("/")
+      }
+    })
+  }, [])
 
   useEffect(() => {
     window.addEventListener('scroll', handleScroll)
@@ -30,6 +48,27 @@ const Nav = () => {
     navigate(`/search?q=${e.target.value}`);
   }
 
+  const handleAuth = () => {
+    signInWithPopup(auth, provider)
+    .then(result => {
+      setUserData(result.user);
+    })
+    .catch(error => {
+      console.log(error);
+    })
+  }
+
+  const handleSignOut = () => {
+    signOut(auth)
+    .then(() => {
+      setUserData({});
+      navigate(`/`);
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+  }
+
   return (
     <NavWrapper show={show}>
         <Logo>
@@ -40,20 +79,66 @@ const Nav = () => {
             />
         </Logo>
         {pathname === "/" ?
-          (<Login>Login</Login>) :
-          <Input
-            value={searchValue}
-            onChange={handleChange}
-            className='nav__input'
-            type='text'
-            placeholder='검색해주세요.' 
-          />
+          (<Login onClick={handleAuth}>Login</Login>) :
+          <>
+            <Input
+              value={searchValue}
+              onChange={handleChange}
+              className='nav__input'
+              type='text'
+              placeholder='검색해주세요.' 
+            />
+
+            <SignOut>
+              <UserImg src={userData.photoURL} alt={userData.displayName} />
+                <DropDown>
+                  <span onClick={handleSignOut}>Sign Out</span>
+                </DropDown>
+            </SignOut>
+          </>
         }
     </NavWrapper>
   )
 }
 
 export default Nav
+
+const DropDown = styled.div`
+  position: absolute;
+  top: 48px;
+  right: 0px;
+  background: rgb(19, 19, 19)
+  border: 1px solid rgba(151, 151, 151, 0.34);
+  border-radius:  4px;
+  box-shadow: rgb(0 0 0 /50%) 0px 0px 18px 0px;
+  padding: 10px;
+  font-size: 14px;
+  letter-spacing: 3px;
+  width: 100%;
+  opacity: 0;
+`;
+
+const SignOut = styled.div`
+  position: relative;
+  height: 48px;
+  width: 48px;
+  display: flex;
+  cursor: pointer;
+  align-items: center;
+  justify-content: center;
+
+  &:hover {
+    ${DropDown} {
+      opacity: 1;
+      transition-duration: 1s;
+    }
+}`;
+
+const UserImg = styled.img`
+  border-radius: 50%;
+  width: 100%;
+  height: 100%;
+`;
 
 const Login = styled.a`
   background-color: rgba(0, 0, 0, 0.6);
